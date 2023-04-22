6a55b3a813665423049b1e1ba356802af54dffd8
murprakoso <prakoso.clan04@gmail.com>
2023-04-22 20:12:16 +0700

    add: pagination in search page

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,10 +1,11 @@
 import axios from "axios";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { Alert } from "react-bootstrap";
 import CardComponent from "../components/PictureCardComponent";
 import Progress from "../components/nprogress/Progress";
 import PaginationComponent from "../components/PaginationComponent";
-import { useLocation } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
+import ReactPaginateComponent from "../components/ReactPaginateComponent";
 
 // query string
 const useQuery = () => {
@@ -12,19 +13,25 @@ const useQuery = () => {
 };
 
 const Search = () => {
-  const [loading, setLoading] = React.useState(false);
-  const [photos, setPhotos] = React.useState([]);
+  const [loading, setLoading] = useState(false);
+  const [photos, setPhotos] = useState([]);
   const query = useQuery().get("q");
+  // paginate
+  const [pageNumber, setPageNumber] = useState(0);
+  const currentPage = useQuery().get("page") || 1;
+  const navigate = useNavigate();
 
+  // get data photos by keyword of search
   useEffect(() => {
     setLoading(true);
-
     const getPhotos = async () => {
       try {
         const res = await axios.get(
-          `http://localhost:4000/api/search?tags=${query}`
+          `http://localhost:4000/api/search?tags=${query}&page=${
+            pageNumber + 1
+          }`
         );
-        setPhotos(res.data.data.photos);
+        setPhotos(res.data.data);
         setLoading(false);
       } catch (err) {}
     };
@@ -32,7 +39,14 @@ const Search = () => {
     if (query !== "") {
       getPhotos();
     }
-  }, [query]);
+  }, [query, pageNumber]);
+
+  // pagination link
+  const handlePageClick = (data) => {
+    const selectedPage = data.selected;
+    setPageNumber(selectedPage);
+    navigate(`/search?q=${query}&page=${selectedPage + 1}`);
+  };
 
   return (
     <>
@@ -45,8 +59,12 @@ const Search = () => {
         <>
           {photos.length !== 0 ? (
             <>
-              <CardComponent photos={photos} />
-              <PaginationComponent />
+              <CardComponent photos={photos.photos} />
+              <ReactPaginateComponent
+                pageCount={photos.total}
+                onPageChange={handlePageClick}
+                forcePage={currentPage - 1}
+              />
             </>
           ) : (
             <Alert variant="danger" className="container-fluid" dismissible>
